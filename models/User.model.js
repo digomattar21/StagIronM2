4fed5c81204975b3710ba283360e6bf5f19f1294
Thiarhes <injesusservice@hotmail.com>
2021-02-13 15:05:44 -0300

    login and signup implemented

--- models/User.model.js
+++ models/User.model.js
@@ -3,7 +3,13 @@ const { Schema, model } = mongoose;
 
 const userSchema = new Schema(
   {
-    name: { type: String, required: [true, `Insira seu nome`] },
+    username: {
+      type: String,
+      required: [true, `Insira seu nome de usuário`],
+      unique: true,
+      trim: true,
+      match: [/^[a-zA-Z0-9_-]{3,15}$/, 'Seu username não pode conter símbolos, nem espaços, nem letras maiúsculas e deve ter entre 3 e 15 caracteres!']
+    },
     email: {
       type: String,
       unique: true,
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,6 +6,47 @@ const bcrypt = require("bcryptjs");
 var saltRounds = 12;
 const nodemailer = require("nodemailer");
 
+router.get("/auth/signup", (req, res) => {
+  res.render("auth/sign-up.hbs");
+});
+
+router.post("/auth/sign-up", async (req, res) => {
+  try {
+    var { username, email, password } = req.body;
+    var salt = await bcrypt.genSalt(saltRounds);
+
+    var alreadyExistsEmail = await User.findOne({ email: email });
+    var passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/;
+
+    var alreadyExistsUser = await User.findOne({ username: username });
+
+    if (alreadyExistsUser != null) {
+      throw new Error('Este usuário já está cadastrado');
+    }
+    if (alreadyExistsEmail != null) {
+      throw new Error(`Este email ja está cadastrado`);
+    } else if (!password || !email || !username) {
+      throw new Error(`Todos os campos são obrigatórios`);
+    } else if (!password.match(passRegex)) {
+      throw new Error(
+        `Sua senha deve conter no mínimo: 8 caracteres, 1 letra maiúscula, 1 letra minúscula, 1 símbolo e 1 número (Levamos sua segurança a sério)`
+      );
+    } else {
+
+      password = await bcrypt.hash(password, salt);
+      let createUser = await User.create({ username, email, password });
+      let user = await User.findOne({ email: email });
+      req.session.currentUser = user;
+      res.render("private/main.hbs", {
+        userInSession: req.session.currentUser,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.render("auth/sign-up.hbs", { msg: e });
+  }
+});
+
 router.get("/auth/login", (req, res) => {
   res.render("auth/login.hbs");
 });
@@ -13,13 +54,13 @@ router.get("/auth/login", (req, res) => {
 router.post("/auth/login", async (req, res) => {
   try {
     console.log("SESSION==>", req.session);
-    const { email, password } = req.body;
+    const { username, password } = req.body;
 
-    if (email === "" || password === "") {
-      throw new Error(`Por favor insira seu email e senha `);
+    if (username === "" || password === "") {
+      throw new Error(`Por favor insira seu nome de usuário e senha `);
     }
 
-    let user = await User.findOne({ email: email });
+    let user = await User.findOne({ username: username });
 
     if (user != null) {
       let validate = await bcrypt.compareSync(password, user.password);
@@ -34,47 +75,11 @@ router.post("/auth/login", async (req, res) => {
         throw new Error(`Senha Incorreta`);
       }
     } else {
-      throw new Error(`Email nao encontrado`);
+      throw new Error(`Nome de usuário nao encontrado`);
     }
   } catch (e) {
     console.log(e);
     res.render("auth/login.hbs", { msg: e });
-  }
-});
-
-router.get("/auth/signup", (req, res) => {
-  res.render("auth/sign-up.hbs");
-});
-
-router.post("/auth/sign-up", async (req, res) => {
-  try {
-    var { name, email, password } = req.body;
-    var salt = await bcrypt.genSalt(saltRounds);
-
-    var alreadyExistsEmail = await User.findOne({ email: email });
-    var passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/;
-
-    if (alreadyExistsEmail != null) {
-      throw new Error(`Este email ja está cadastrado`);
-    } else if (!password || !email || !name) {
-      throw new Error(`Todos os campos são orbigatórios`);
-    } else if (!password.match(passRegex)) {
-      throw new Error(
-        `Sua senha deve conter no mínimo: 8 caracteres, 1 letra maiúscula, 1 letra minúscula, 1 símbolo e 1 número (Levamos sua segurança a sério)`
-      );
-    } else {
-      
-      password = await bcrypt.hash(password, salt);
-      let createUser = await User.create({ name, email, password });
-      let user = await User.findOne({ email: email });
-      req.session.currentUser = user;
-      res.render("private/main.hbs", {
-        userInSession: req.session.currentUser,
-      });
-    }
-  } catch (e) {
-    console.log(e);
-    res.render("auth/sign-up.hbs", { msg: e });
   }
 });
 
