4a419107e5cf4c77b808ac71d3b1662695874212
digomattar21 <digomattar@gmail.com>
2021-02-16 15:59:38 -0300

    fixed code that had bugs

--- routes/private.routes.js
+++ routes/private.routes.js
@@ -4,8 +4,6 @@ const router = express.Router();
 const session = require("express-session");
 const Article = require("../models/Article.model");
 const User = require("../models/User.model");
-// const yf = require("yahoo-finance");
-// const nodemailer = require("nodemailer");
 
 router.get("/private/createArticle", (req, res) => {
   res.render("private/createArticle.hbs", {
@@ -16,31 +14,49 @@ router.get("/private/createArticle", (req, res) => {
 
 router.post("/private/createArticle", async (req, res, nxt) => {
   try {
-    const { title, category, imgPath, message, author } = req.body;
-    const id = req.session.currentUser.id;
+
+    const { title, category, imgPath, content, author } = req.body;
+
+    const id = req.session.currentUser._id;
+
 
     let userPost = await Article.create({
       title,
       category,
       imgPath,
-      message,
+      content,
       author,
     });
 
     let updated = await User.findByIdAndUpdate(id, {
-      $push: { articles: userPost.id },
+      $push: { articles: userPost._id },
     });
 
     let articlesFromDB = await Article.find();
 
-    res.render("private/main.hbs", {
+    res.render('private/main.hbs', {
+      layout: false,
       articles: articlesFromDB,
-      userInSession: req.session.currentUser,
-    });
+      userInSession: req.session.currentUser
+    })
+
   } catch (e) {
     console.log(e);
   }
 });
+
+router.get('/private/main', (req, res) => {
+  const id = req.session.currentUser._id;
+
+  Article.find()
+    .populate(id)
+    .then(dbArticle => res.render('private/main.hbs', {
+      articles: dbArticle,
+      userInSession: req.session.currentUser,
+      layout: false,
+    }))
+    .catch(e => console.log(`Error while getting articles from DB: ${e}`));
+})
 
 router.get("/private/minha-carteira", (req, res) => {
   res.render("private/minha-carteira.hbs", { layout: false });
@@ -50,21 +66,23 @@ router.post("/private/ticker-search", (req, res) => {
   //implementar search de ticker na area privada
 });
 
-router.get("/private/main", (req, res) => {
-    const id = req.session.currentUser._id;
-    Article.find()
-      .populate(id)
-      .then((allArticlesFromDB) =>
-        res.render("private/main.hbs", {
-          articles: allArticlesFromDB,
-          userInSession: req.session.currentUser,
-          layout: false,
-        })
-      )
-      .catch((e) => console.log(`Error while getting articles from DB: ${e}`));
 
-    
-  
-});
+router.get('/private/main/:articleId', (req, res) => {
+  const id = req.session.currentUser._id;
+  const { articleId } = req.params;
+
+  Article.findById(articleId)
+    .populate(id)
+    .then(foundArticle => {
+      console.log(foundArticle)
+      res.render('private/article-detail.hbs', {
+        article: foundArticle,
+        userInSession: req.session.currentUser,
+        layout: false,
+      })
+    })
+    .catch(err => console.log(`Error while getting the details about this article: ${err}`));
+})
+
 
 module.exports = router;
