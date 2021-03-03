0ae665354c9e4ac3b32ace65a367ce4902873e78
digomattar21 <digomattar@gmail.com>
2021-03-03 15:30:13 -0300

    fixed bugs and updated home layout

--- routes/articleAuthor.routes.js
+++ routes/articleAuthor.routes.js
@@ -54,18 +54,18 @@ router.post("/private/createArticle", async (req, res, nxt) => {
 
 router.get("/private/main/:articleId", async (req, res) => {
   try {
-    //await News.deleteMany({country:{$eq:"related"}});
+    await News.deleteMany({country:{$eq:"related"}});
     const { articleId } = req.params;
     let article = await Article.findById(articleId).populate("author likes comments");
     let author = await User.findById(article.author._id).populate('settings');
     console.log(author)
     let category = article.category;
 
-    // if (category === 'main' || category === 'comprar ou vender') {
-    //   await getGeneralNews()
-    // } else {
-    //   await getSpecificNews(category)
-    // }
+    if (category === 'main' || category === 'comprar ou vender') {
+      await getGeneralNews()
+    } else {
+      await getSpecificNews(category)
+    }
 
     let news = await News.find({ country: { $eq: 'related' } })
     let comments = await Comment.find({ article: article._id }).populate(
@@ -90,7 +90,7 @@ router.get("/private/main/:articleId", async (req, res) => {
 
 router.get("/article/main/:articleId", async (req, res) => {
   try {
-    //await News.deleteMany({country:{$eq:"related"}});
+    await News.deleteMany({country:{$eq:"related"}});
     const { articleId } = req.params;
     let article = await Article.findById(articleId).populate("author likes comments");
     let author = await User.findById(article.author._id).populate('settings');
@@ -100,11 +100,11 @@ router.get("/article/main/:articleId", async (req, res) => {
     );
     let category = article.category;
 
-    // if (category === 'main' || category === 'comprar ou vender') {
-    //   await getGeneralNews()
-    // } else {
-    //   await getSpecificNews(category)
-    // }
+    if (category === 'main' || category === 'comprar ou vender') {
+      await getGeneralNews()
+    } else {
+      await getSpecificNews(category)
+    }
 
     let news = await News.find({ country: { $eq: "related" } });
 
